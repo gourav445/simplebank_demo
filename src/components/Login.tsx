4c0433efa4df1f726533e8e962413d5459db429f
Dhruv <dpat37@gmail.com>
2022-07-30 02:04:23 -0400

    Init

---
+++ src/components/Login.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Box, Button, ButtonGroup, Paper, styled} from "@mui/material";
+import {Cookies} from "react-cookie";
+import { useNavigate } from "react-router-dom";
+
+const Users = ["user1", "user2"];
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.h2,
+    textAlign: 'center',
+    color: theme.palette.text.primary,
+    height: 30,
+    lineHeight: '30px',
+    margin: '30px'
+}));
+
+function Login(){
+    let navigate = useNavigate();
+    const handleUserButtonClick = (e: React.MouseEvent<HTMLElement>) => {
+        const cookies = new Cookies();
+        cookies.set('uid', e.currentTarget.textContent, {path: '/'});
+        console.log("User: " + cookies.get('uid') + " welcome!!");
+        navigate("/")
+    };
+
+    return (
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            '& > *': {
+                m: 1,
+            },
+        }}>
+                <Item key="Title" elevation={0}>Select a User!</Item>
+                <ButtonGroup size="large" aria-label="large user selection group" color="info">
+                    {
+                        Users.map((user) => (<Button onClick={(e) => handleUserButtonClick(e)} key={user}>{user}</Button>))
+                    }
+                </ButtonGroup>
+        </Box>
+    );
+}
+
+export default Login;
